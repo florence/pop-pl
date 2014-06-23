56a17e3de7ab9120432aa3e266139ea5b5bf659f
Spencer Florence <florence@ccs.neu.edu>
2014-06-23 16:09:03 -0500

    started on evaluation

--- pl6/runtime.rkt
+++ pl6/runtime.rkt
@@ -31,9 +31,11 @@
     [(_ id:id v:expr s:expr)
      #'(in:update 'id v s)]))
 (define (in:update n v [s (hash)])
-  (when (hash-has-key state-new n)
-    (hash-set s n v)
-    (error 'update "non existant field")))
+  (unless (hash-has-key state-new n)
+    (error 'update "non existant field"))
+  (when (hash-has-key s n)
+    (error 'update "duplicate set"))
+  (hash-set s n v))
 
 (define-match-expander state (make-rename-transformer #'hash-table))
 ;;;;; handlers
@@ -68,7 +70,39 @@
          (define (id . args)
            (msg 'id args)) ...)]))
 
+;;;;; evaluation
+(define (evaluate event)
+  (evaluate-event! event)
+  (evaluate-responses (evalute-handlers event)))
 
+(define (evaluate-event! event)
+  (match event
+    [(extern id value)
+     (set! state-new (hash-update state-new id (const value)))]
+    [_ (void)]))
+
+(define (evaluate-handlers event)
+  (define-values (updates messages)
+    (for/fold ([update (hash)] [messages null]) ([h handlers])
+      (define-values (u m) (h state-old state-new event))
+      (values (merge u update) (append messages m))))
+  (set! state-old state-new)
+  (set! state-new (replace updates state-new))
+  messages)
+(define (merge u1 u2)
+  (for/fold ([merged u1]) ([(id value) (in-hash u2)])
+    (update id value merged)))
+(define (replace update current)
+  (for/fold ([result current]) ([(id value) (in-hash update)])
+    (hash-set result id value)))
+
+(define (evaluate-responses rs)
+  (for/fold ([outgoing null]) ([resp rs])
+    (match resp 
+      [(new-handler f)
+       (add-handler! f)
+       outgoing]
+      [_ (cons resp outgoing)])))
 
 ;;;;; aux
 (define-syntax (cons! stx)
