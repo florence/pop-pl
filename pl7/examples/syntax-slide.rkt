0d68fa79bd4b852b4143f6a0f9908df35fab44e0
Steven Belknap <smbelknap@icloud.com>
2014-07-30 21:54:17 -0500

    spell changes

--- pl7/examples/syntax-slide.rkt
+++ pl7/examples/syntax-slide.rkt
@@ -55,8 +55,8 @@
   @list{                          @syntax{|}       decrease @const{"heprin"} @const{3}units/kg/hour})
  (make-item
   @list{@syntax{handler} heprin-ptt-checking @syntax{is}}
-  @list{    stable @syntax{means} most-recent @const{2} ptt @kw{that-are:} theripudic since-last@delim{(}change@delim{(}@const{"heprin"}@delim{)}@delim{)}}
-  @list{    theripudic @syntax{means a} ptt @syntax{where} @const{60} @op{<} a-ptt @op{<} @const{100}}
+  @list{    stable @syntax{means} most-recent @const{2} ptt @kw{that-are:} therapeuticc since-last@delim{(}change@delim{(}@const{"heprin"}@delim{)}@delim{)}}
+  @list{    therapeutic @syntax{means a} ptt @syntax{where} @const{60} @op{<} a-ptt @op{<} @const{100}}
   @list{    Q6 check-ptt @syntax{whenever} not stable}
   @list{    Q24 check-ptt @syntax{whenever} stable}))
 (slide
