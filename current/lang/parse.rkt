5198ca7ca7bfc8977dc9b430bba7a9798f0e2b4b
Spencer Florence <florence@ccs.neu.edu>
2014-08-12 15:23:33 -0500

    added ability to multi line expressins in multi clause whenevers

--- current/lang/parse.rkt
+++ current/lang/parse.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (provide parse lex)
 (require (for-syntax syntax/parse))
-(require "../packrat.rkt" syntax/parse)
+(require "../packrat.rkt" syntax/parse syntax/readerr)
 (module+ test (require rackunit))
 
 (define TAB-WIDTH 4)
@@ -69,6 +69,7 @@
   (if (null? rst)
       result
       (error 'internal-error "expected null, got ~s result" rst)))
+
 (module+ test
   (check-equal? (syntax->datum
                  (datum->syntax
@@ -123,16 +124,21 @@
                    [(t e) #f]))))
   (values (syntax-parse (list* start bits)
             [((t e1) (e2) ...)
-             (syntax/loc start (t e1 e2 ...))])
+             (define (stx->line stx)
+               (syntax-parse stx
+                 #:datum-literals (line)
+                 [(line n e)
+                  `(line ,(syntax->datum #'n) ,#'e)]))
+             (quasisyntax/loc start (t #,@(join-lines (map stx->line (syntax->list #'(e1 e2 ...))))))])
           remainder))
 (module+ test 
-  (check-equal? (map syntax->datum (flatten-parts (list #'(t1 1) #'(2) #'(3) #'(t2 4) #'(t3 5))))
+  (check-equal? (map syntax->datum (flatten-parts (list #'(t1 (line 0 1)) #'((line 5 2)) #'((line 4 3)) #'(t2 (line 12 4)) #'(t3 (line 6 5)))))
                 '((t1 1 2 3) (t2 4) (t3 5))))
 ;; parse a litteral line from the part
 (define parse-whenever-part
   (match-lambda
-   [(list t _ _ _ e) `(,t ,e)]
-   [(list _ _ e) `(,e)]))
+   [(list t _ _ e) `(,t ,e)]
+   [(list _ e) `(,e)]))
 ;;
 (define parse-means
   (match-lambda
@@ -142,6 +148,13 @@
      [(list _ _ name _ _)
       `(add-handler ,name)]))
 
+(define (raise-parse-error p m)
+  (raise-read-error m
+                    (position-source p)
+                    (position-line p)
+                    (position-col p)
+                    (position-start p)
+                    (position-span p)))
 (define-parser/colorer (parse lex)
   [Top (:seq (->stx
               (compose
@@ -152,7 +165,7 @@
               (:+ no-op (:seq (lambda (r p) (second r)) (list (:? no-op NEWLINE) (:/  (list Require COMMENT Initially Handler Message)))))
               :EOF))]
   [COMMENT (:rx (->stx (const '(void)))
-                #rx"//.*?\n")]
+                #rx"//.*?(\n|$)")]
   
   [Require (:seq (->stx parse-require)
                  (list REQUIRE WHITESPACE ID (:? no-op WHITESPACE) END))]
@@ -168,7 +181,10 @@
           (:seq (lambda (r p) (apply string-append (flatten r)))
                 (list NEWLINE SPACING END))
           (:seq (lambda (r p) (parse-line r))
-                (list INDENTATION (:/ (list Whenever Means Expr)) END))))]
+                (list INDENTATION Line-Body END))
+          ;; errors
+          (:seq (lambda (r p) (raise-parse-error p "a line must start with indentation"))
+                (list (:/ (list Whenever Means Expr)) END))))]
   [Whenever (:/
              (list (:seq (->stx parse-whenever+parts) (list WHENEVER ?WHITESPACE END (:+ no-op (:seq no-op (list INDENTATION WheneverPart END)))))
                    (:seq (->stx parse-whenever) (list WHENEVER WHITESPACE NEW WHITESPACE ID))
@@ -177,8 +193,12 @@
   [Means (:seq (->stx parse-means) (list ID WHITESPACE MEANS WHITESPACE Expr))]
   [WheneverPart (:/ 
                  (list 
-                  (:seq (->stx parse-whenever-part) (list Expr WHITESPACE PIPE WHITESPACE Expr))
-                  (:seq (->stx parse-whenever-part) (list PIPE WHITESPACE Expr))))]
+                  (:seq (->stx parse-whenever-part) (list Expr WHITESPACE PIPE Line-Like))
+                  (:seq (->stx parse-whenever-part) (list PIPE Line-Like))))]
+  [Line-Like (:seq (lambda (r p) (parse-line r))
+                   (list SPACING Line-Body END))]
+
+  [Line-Body (:/ (list Whenever Means Expr))]
 
   [INDENTATION (:seq (lambda (r p) (apply string-append r)) 
                      (list NEWLINE SPACING))]
@@ -207,7 +227,7 @@
   ;;; expressions
   [Expr (:/
          (list
-          Numberic
+          Infix
           Call
           ID))]
   
@@ -222,8 +242,9 @@
             (:seq (lambda (r p) (match r [(list _ k _ e) (list k e)])) (list WHITESPACE KEYWORD WHITESPACE Expr))
             (:seq (->stx second) (list WHITESPACE Expr)))))]
   
-  ;;  math
-  [Numberic (:/ (list Number Todo))]
+  ;; infix 
+
+  [Infix (:/ (list Number Todo))]
   [Number (:/
            (list Number+Unit
                  NUMBER-RAW))]
@@ -281,6 +302,7 @@
   [OPEN-PAREN "("]
   [CLOSE-PAREN ")"]
   [NEW "new"]
+  [COMMA ","]
   ;; silly
   [Todo (:! (:? no-op (:rx no-op #rx".")))]
   [LANG (:seq no-op (list "#lang" (:rx no-op #rx".*\n")))]
@@ -288,7 +310,7 @@
   #:tokens 
   (comment LANG COMMENT) 
   (other REQUIRE MESSAGE IS OPEN-BRACKET CLOSE-BRACKET WHENEVER HANDLER INITIALLY MEANS PIPE AFTER
-         OP FUNCTION NEW) 
+         OP FUNCTION NEW COMMA) 
   (white-space NEWLINE WHITESPACE) 
   (constant STRING INCOMPLETE-STRING Number Unit) 
   (keyword KEYWORD)
@@ -306,7 +328,10 @@
           e:expr ...)
        #`(let ([val (parse t 
                            #,@(if (attribute d) #'(#:debug d) #'())
-                           #,@(if (attribute p) #'(#:pattern p) #'()))]) 
+                           #,@(if (attribute p) #'(#:pattern p) #'()))]
+               [res #,@(if (attribute p) 
+                           #'('e ...)
+                           #'((module 'e ...)))]) 
            (define (convert v)
              (cond [(syntax? v)
                     (syntax->datum v)]
@@ -314,12 +339,10 @@
                     (map convert v)]
                    [else v]))
            (if (not val)
-               (fail (~a "parsing " t " returned false"))
+               (fail (~a "parsing " t " returned false, expected " res))
                #,(quasisyntax/loc stx
                    (check-equal? (convert val)
-                                 #,@(if (attribute p) 
-                                       #'('e ...)
-                                       #'((module 'e ...)))
+                                 res 
                                  t))))]))
   (test-parse "message test is [ a b: c ]"
               (define test (make-message a #:b c)))
@@ -341,6 +364,10 @@
               (initially x))
   (test-parse "require m"
               (add-handler m))
+  (test-parse "initially\n whenever\n x | whenever x\n |  y"
+              (initially
+               (whenever
+                [x (whenever x y)])))
   (test-parse 
    "handler b is
   QQ
