f0d0ed83b22b606fac510ef9258511263cf8d84d
Spencer Florence <florence@ccs.neu.edu>
2014-11-13 20:38:15 -0600

    less deps

--- info.rkt
+++ info.rkt
@@ -1,10 +1,8 @@
 #lang info
 (define collection "pop-pl")
 (define deps '("base"
-               "draw-lib"
-               "draw-lib"
                "gui-lib"
-               "plot-gui-lib"))
+               "pict-lib"))
 (define build-deps
   '("rackunit-lib"
     "typed-racket-lib"))
